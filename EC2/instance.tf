ecaf5d08dfb2abfa1e544d4a867b95b300e2ae47
Jesper Jeansson <jesper.jeansson@gmail.com>
2022-10-10 20:01:50 +0000

    EC2 AMI change, SSH, keypair

--- EC2/instance.tf
+++ EC2/instance.tf
@@ -1,10 +1,53 @@
 provider "aws" {
-    access_key = ""
-    secret_key = ""
+    access_key = var.access_key
+    secret_key = var.secret_key
     region = "us-east-1"
 }
 
 resource "aws_instance" "test1" {
-  ami = "ami-13be557e"
+  ami = "ami-026b57f3c383c2eec"
   instance_type = "t2.micro"
+  key_name = "TF_key"
+  vpc_security_group_ids = [aws_security_group.demo-sg.id]
+}
+
+resource "aws_key_pair" "TF_key" {
+  key_name   = "TF_key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "TF-key" {
+    content  = tls_private_key.rsa.private_key_pem
+    filename = "tfkey"
+}
+
+resource "aws_security_group" "demo-sg" {
+  name = "sec-grp"
+  description = "Allow HTTP and SSH traffic via Terraform"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
---
+++ EC2/variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  description = "AWS access_key"
+  type        = string
+  sensitive   = true
+}
+
+variable "secret_key" {
+  description = "AWS secret_key"
+  type        = string
+  sensitive   = true
+}
